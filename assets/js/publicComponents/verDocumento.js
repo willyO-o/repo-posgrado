3a6cab5bfff409483545b2643b25b83ec0b4d5b1
Willy <marcggmarc.20@gmaill.com>
2022-03-31 12:33:31 -0400

    vista pdf publico

--- assets/js/publicComponents/verDocumento.js
+++ assets/js/publicComponents/verDocumento.js
@@ -88,22 +88,25 @@ export default {
 								</div>
 								/
 								<div class="page-display"> 
-									1
+									99911
 								</div>
 							</div>
 							<div  class="zoom-buttons">
-								<div class="btn-search"> 
-									<i class="fas fa-search-minus"></i>
+								<div class="btn-search" @click="alejarPdf()"> 
+									<i class="fas fa-minus"></i>
 								</div>
-								<div class="btn-search"> 
-									<i class="fas fa-search-plus"></i>
+								<div class="btn-search" @click="toggleZoom()"> 
+									<i class="fas " :class="{'fa-search-minus':zoom, 'fa-search-plus':!zoom}"></i>
+								</div>
+								<div class="btn-search" @click="ampliarPdf()"> 
+									<i class="fas fa-plus"></i>
 								</div>
 							</div>
 						</div>
 					</div>
 					
 					
-				<div class="modalbox resize">
+				<div class="modalbox resize" id="resize">
 
 					<canvas id="the-canvas"></canvas>
 				</div>
@@ -119,14 +122,28 @@ export default {
             uuid: this.$route.params.name,
             url: base_url,
             documento: {},
-            modal: false
+            modal: false,
+            sizePdf: 800,
+            maxSizePdf: 2200,
+            sizePerClick: 200,
+            minSizePdf: 800,
+            zoom: false,
+            //datos pdfjs
 
 
+            pdfDoc: null,
+            pageNum: 1,
+            pageRendering: false,
+            pageNumPending: null,
+            scale: 0.8,
+            canvas: null,
+            ctx: null
         }
     },
     mounted() {
         this.getDocumento();
-
+        this.canvas = document.getElementById('the-canvas');
+        this.ctx = this.canvas.getContext('2d');
     },
 
     methods: {
@@ -158,6 +175,34 @@ export default {
             this.modal = false
             console.log("cerrar");
             document.body.classList.remove("modal-open");
+        },
+        ampliarPdf() {
+
+            if (this.sizePdf < this.maxSizePdf) {
+                console.log("ampliar");
+                this.sizePdf += this.sizePerClick
+                document.querySelector(".modalmask.open #resize").setAttribute("style", `width:${this.sizePdf}px;`)
+
+            }
+        },
+        toggleZoom() {
+            if (this.sizePdf < this.maxSizePdf) {
+                this.sizePdf = this.maxSizePdf
+                this.zoom = true
+            } else {
+                this.sizePdf = this.minSizePdf
+                this.zoom = false
+            }
+            document.querySelector(".modalmask.open #resize").setAttribute("style", `width:${this.sizePdf}px;`)
+        },
+        alejarPdf() {
+            console.log(this.sizePdf);
+            if (this.sizePdf > this.minSizePdf) {
+                console.log("alejar");
+                this.sizePdf -= this.sizePerClick
+                document.querySelector(".modalmask.open #resize").setAttribute("style", `width:${this.sizePdf}px;`)
+
+            }
         },
 
         inicializarPdf() {
@@ -206,6 +251,95 @@ export default {
             });
         },
 
+
+        inicializarPdf1() {
+            let pdf_url = base_url + 'uploads/' + this.documento.nombre;
+            // var url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf';
+
+            // Loaded via <script> tag, create shortcut to access PDF.js exports.
+            var pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+            // The workerSrc property shall be specified.
+            pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+
+            function renderPage(num) {
+                pageRendering = true;
+                // Using promise to fetch the page
+                this.pdfDoc.getPage(num).then(function(page) {
+                    var viewport = page.getViewport({ scale: scale });
+                    canvas.height = viewport.height;
+                    canvas.width = viewport.width;
+
+                    // Render PDF page into canvas context
+                    var renderContext = {
+                        canvasContext: ctx,
+                        viewport: viewport
+                    };
+                    var renderTask = page.render(renderContext);
+
+                    // Wait for rendering to finish
+                    renderTask.promise.then(function() {
+                        pageRendering = false;
+                        if (pageNumPending !== null) {
+                            // New page rendering is pending
+                            renderPage(pageNumPending);
+                            pageNumPending = null;
+                        }
+                    });
+                });
+
+                // Update page counters
+                document.getElementById('page_num').textContent = num;
+            }
+
+            /**
+             * If another page rendering in progress, waits until the rendering is
+             * finised. Otherwise, executes rendering immediately.
+             */
+            function queueRenderPage(num) {
+                if (pageRendering) {
+                    pageNumPending = num;
+                } else {
+                    renderPage(num);
+                }
+            }
+
+            /**
+             * Displays previous page.
+             */
+            function onPrevPage() {
+                if (this.pageNum <= 1) {
+                    return;
+                }
+                this.pageNum--;
+                queueRenderPage(this.pageNum);
+            }
+            document.getElementById('prev').addEventListener('click', onPrevPage);
+
+            /**
+             * Displays next page.
+             */
+            function onNextPage() {
+                if (this.pageNum >= this.pdfDoc.numPages) {
+                    return;
+                }
+                this.pageNum++;
+                queueRenderPage(this.pageNum);
+            }
+            document.getElementById('next').addEventListener('click', onNextPage);
+
+            /**
+             * Asynchronously downloads PDF.
+             */
+            pdfjsLib.getDocument(pdf_url).promise.then(function(pdfDoc_) {
+                this.pdfDoc = pdfDoc_;
+                document.getElementById('page_count').textContent = this.pdfDoc.numPages;
+
+                // Initial/first page rendering
+                renderPage(this.pageNum);
+            });
+        },
 
 
 
