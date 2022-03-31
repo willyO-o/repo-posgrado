402b3e4017953cfb77681c9ada72e822f1b6125b
Willy <marcggmarc.20@gmaill.com>
2022-03-31 01:49:24 -0400

    vista pdf publico

--- assets/js/publicComponents/verDocumento.js
+++ assets/js/publicComponents/verDocumento.js
@@ -1,7 +1,6 @@
-
 export default {
-	template: //html
-		`
+    template: //html
+        `
 		<div class="event_items">
 			<div class="row">
 				<div class="col-12">
@@ -72,12 +71,40 @@ export default {
 
 			</div>
 			<div id="modal3" class="modalmask"  :class="{open:modal, hide:!modal}">
-
-				<div class="modalbox resize">
-					<a  title="Close" class="close" @click="cerrarModal()" >X</a>
-					<div class="tool-bar">
-						barra de herramientoas
+				<div class="tool-bar">
+				
+					<div></div>
+					<div class="modal-document-tittle"><i class="fas fa-file-pdf"></i> Ver Documento PDF</div>
+					<a  title="Close" class="modal-close" @click="cerrarModal()" ><i class="fas fa-times"></i></a>
+				</div>
+					<div class="modal-btn next"><i class="fas fa-chevron-circle-right"></i></div>
+					<div class="modal-btn prev"><i class="fas fa-chevron-circle-left"></i></div>
+					<div class="container-zoom-bar">
+						<div id="zoom-bar" class="zoom-bar">
+							<div  class="paginate">
+								<div class="text-paginate">Pagina</div>
+								<div class="page-display"> 
+									1
+								</div>
+								/
+								<div class="page-display"> 
+									1
+								</div>
+							</div>
+							<div  class="zoom-buttons">
+								<div class="btn-search"> 
+									<i class="fas fa-search-minus"></i>
+								</div>
+								<div class="btn-search"> 
+									<i class="fas fa-search-plus"></i>
+								</div>
+							</div>
+						</div>
 					</div>
+					
+					
+				<div class="modalbox resize">
+
 					<canvas id="the-canvas"></canvas>
 				</div>
 			</div>
@@ -87,102 +114,104 @@ export default {
 
 	`,
 
-	data() {
-		return {
-			uuid: this.$route.params.name,
-			url: base_url,
-			documento: {},
-			modal: false
+    data() {
+        return {
+            uuid: this.$route.params.name,
+            url: base_url,
+            documento: {},
+            modal: false
 
 
-		}
-	},
-	mounted() {
-		this.getDocumento();
+        }
+    },
+    mounted() {
+        this.getDocumento();
 
-	},
+    },
 
-	methods: {
-		getDocumento() {
-			let fm = new FormData()
-			fm.append("uuid", this.uuid)
-			axios.post(this.url + 'publico/documento_uuid', fm)
-				.then(res => {
-					if (res.data.existe == true) {
-						this.documento = Object.assign({}, res.data.documento)
-						console.log(res.data.documento);
+    methods: {
+        getDocumento() {
+            let fm = new FormData()
+            fm.append("uuid", this.uuid)
+            axios.post(this.url + 'publico/documento_uuid', fm)
+                .then(res => {
+                    if (res.data.existe == true) {
+                        this.documento = Object.assign({}, res.data.documento)
+                        console.log(res.data.documento);
 
-					} else {
-						this.$router.push('/404')
-					}
+                    } else {
+                        this.$router.push('/404')
+                    }
 
-				})
-				.catch(err => {
-					console.error(err);
-				})
-		},
-		abrirModal() {
-			this.modal = true
-			console.log("abrir");
-			this.inicializarPdf() 
-		},
-		cerrarModal() {
-			this.modal = false
-			console.log("cerrar");
-		},
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        },
+        abrirModal() {
+            this.modal = true
+            console.log("abrir");
+            document.body.classList.add("modal-open");
+            this.inicializarPdf()
+        },
+        cerrarModal() {
+            this.modal = false
+            console.log("cerrar");
+            document.body.classList.remove("modal-open");
+        },
 
-		inicializarPdf() {
-			let pdf_url = base_url + 'uploads/'+this.documento.nombre;
+        inicializarPdf() {
+            let pdf_url = base_url + 'uploads/' + this.documento.nombre;
 
-			// Loaded via <script> tag, create shortcut to access PDF.js exports.
-			let pdfjsLib = window['pdfjs-dist/build/pdf'];
+            // Loaded via <script> tag, create shortcut to access PDF.js exports.
+            let pdfjsLib = window['pdfjs-dist/build/pdf'];
 
-			// The workerSrc property shall be specified.
-			pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+            // The workerSrc property shall be specified.
+            pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
 
-			// Asynchronous download of PDF
-			let loadingTask = pdfjsLib.getDocument(pdf_url);
-			loadingTask.promise.then(function (pdf) {
-				console.log('PDF loaded');
+            // Asynchronous download of PDF
+            let loadingTask = pdfjsLib.getDocument(pdf_url);
+            loadingTask.promise.then(function(pdf) {
+                console.log('PDF loaded');
 
-				// Fetch the first page
-				let pageNumber = 1;
-				pdf.getPage(pageNumber).then(function (page) {
-					console.log('Page loaded');
+                // Fetch the first page
+                let pageNumber = 1;
+                pdf.getPage(pageNumber).then(function(page) {
+                    console.log('Page loaded');
 
-					let scale = 1;
-					let viewport = page.getViewport({
-						scale: scale
-					});
+                    let scale = 15;
+                    let viewport = page.getViewport({
+                        scale: scale
+                    });
 
-					// Prepare canvas using PDF page dimensions
-					let canvas = document.getElementById('the-canvas');
-					let context = canvas.getContext('2d');
-					canvas.height = viewport.height;
-					canvas.width = viewport.width;
+                    // Prepare canvas using PDF page dimensions
+                    let canvas = document.getElementById('the-canvas');
+                    let context = canvas.getContext('2d');
+                    canvas.height = viewport.height;
+                    canvas.width = viewport.width;
 
-					// Render PDF page into canvas context
-					let renderContext = {
-						canvasContext: context,
-						viewport: viewport
-					};
-					let renderTask = page.render(renderContext);
-					renderTask.promise.then(function () {
-						console.log('Page rendered');
-					});
-				});
-			}, function (reason) {
-				// PDF loading error
-				console.error(reason);
-			});
-		},
-
-
-
-
+                    // Render PDF page into canvas context
+                    let renderContext = {
+                        canvasContext: context,
+                        viewport: viewport
+                    };
+                    let renderTask = page.render(renderContext);
+                    renderTask.promise.then(function() {
+                        console.log('Page rendered');
+                    });
+                });
+            }, function(reason) {
+                // PDF loading error
+                console.error(reason);
+            });
+        },
 
 
 
-	},
+
+
+
+
+    },
 
 }
